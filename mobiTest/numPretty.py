397d3343436caab0d1661e6e3d11bce24ad62c9d
CT-Kyle <ckyle@smu.edu>
2016-01-15 10:28:20 -0600

    1st

--- mobiTest/numPretty.py
+++ mobiTest/numPretty.py
@@ -1,1 +1,9 @@
 #This program will take numbers and make them look all pretty
+
+print "********** Welcome to the Number Prettifier **********"
+#print "Enter a numerical value in the trillions or less: "
+rawNum = raw_input('Enter a numerical value in the trillions or less: ')
+
+print type(rawNum), rawNum
+rawNum = int(rawNum)
+print type(rawNum), rawNum
